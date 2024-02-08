3220c1e04261b6778a93a9185342b4a09c16bcb4
Ragib58 <ragibshahriar58@gmail.com>
2024-02-08 16:59:28 +0600

    News and Image fetch

---
+++ script.js
@@ -0,0 +1,37 @@
+const API_KEY = "d5327ff735ae43368e8bfd25419c00d7";
+
+const url = "https://newsapi.org/v2/everything?q=";
+
+window.addEventListener('load', () => fetchNews("Bangladesh"));
+
+async function fetchNews (query) {
+    const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
+    const data = await res.json();
+    bindData(data.articles);
+}
+
+function bindData(articles) {
+    const cardsContainer = document.getElementById('cards-container');
+    const newsCardTemplate = document.getElementById('template-news-card');
+
+    cardsContainer.innerHTML = '';
+
+    articles.forEach(article => {
+        if(!article.urlToImage) return;
+        const cardClone = newsCardTemplate.content.cloneNode(true);
+        fillDataInCard(cardClone, article);
+        cardsContainer.appendChild(cardClone);
+    });
+}
+
+function fillDataInCard(cardClone, article) {
+    const newsImg = cardClone.querySelector('#news-img');
+    const newsTitle = cardClone.querySelector('#news-title');
+    const newsSource = cardClone.querySelector('#news-source');
+    const newsDesc = cardClone.querySelector('#news-desc');
+
+    newsImg.src = article.urlToImage;
+    newsTitle.innerHTML = article.title;
+    newsDesc.innerHTML = article.description;
+    
+}
