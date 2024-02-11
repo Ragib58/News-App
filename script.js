deb2c1c549a681bd767a86f697a08f045cfc8d79
Ragib58 <ragibshahriar58@gmail.com>
2024-02-11 11:23:58 +0600

    nav item and search result functional

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener('load', () => fetchNews("Bangladesh"));
 
+function reload() {
+    window.location.reload();
+}
 async function fetchNews (query) {
     const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
     const data = await res.json();
@@ -34,4 +37,33 @@ function fillDataInCard(cardClone, article) {
     newsTitle.innerHTML = article.title;
     newsDesc.innerHTML = article.description;
     
+    const date = new Date(article.publishedAt).toLocaleString("en-US", {
+        timeZone: "Asia/Dhaka"
+    });
+
+    newsSource.innerHTML = `${article.source.name} - ${date} `;
+
+    cardClone.firstElementChild.addEventListener("click", () => {
+        window.open(article.url, "_blank");
+    });
 }
+
+let curSelectedNav = null;
+function onNavItemClick(id) {
+    fetchNews(id);
+    const navItem = document.getElementById(id);
+    curSelectedNav?.classList.remove('active');
+    curSelectedNav = navItem;
+    curSelectedNav.classList.add('active');
+}
+
+const searchButton = document.getElementById('search-button');
+const searchText = document.getElementById('search-text');
+
+searchButton.addEventListener('click', ()=> {
+    const query = searchText.value;
+    if(!query) return;
+    fetchNews(query);
+    curSelectedNav?.classList.remove("active");
+    curSelectedNav = null;
+})
